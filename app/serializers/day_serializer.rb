cb23986fa7586109e54eec7193982464a3dd3e0c
s-blais <stephen@s-blais.com>
2021-03-08 13:52:57 -0500

    Add array of day.birds ids to day_serializer

--- app/serializers/day_serializer.rb
+++ app/serializers/day_serializer.rb
@@ -1,4 +1,17 @@
 class DaySerializer
   include JSONAPI::Serializer
   attributes :date
+  
+  # has_many :birds
+  
+  # attribute :birds do |day|
+  #   day.birds
+  # end
+  
+  attribute :birds_ids do |day|
+    day.birds.map do |bird|
+      bird.id
+    end
+  end
+  
 end
